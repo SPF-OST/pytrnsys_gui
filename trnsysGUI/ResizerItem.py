e65893b017043a997ed13374cc8809850479df1a
Stefano Marti <stefano.marti@spf.ch>
2020-01-09 09:22:04 +0100

    Improved resize handle a bit

--- trnsysGUI/ResizerItem.py
+++ trnsysGUI/ResizerItem.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore
-from PyQt5.QtCore import QRectF, QSize
+from PyQt5.QtCore import QRectF, QSize, QPointF
 from PyQt5.QtGui import QBrush, QColor, QPen
 from PyQt5.QtWidgets import QGraphicsEllipseItem
 
@@ -17,12 +17,18 @@ class ResizerItem(QGraphicsEllipseItem):
 
     def itemChange(self, change, value):
         if change == self.ItemPositionChange:
-            print("relative pos has changed")
+            # print("relative pos has changed")
             self.parent.setItemSize(self.pos().x(), self.pos().y())
             self.parent.updateImage()
+            return QPointF(value.x(), value.x())
 
         return super(ResizerItem, self).itemChange(change, value)
 
-    def mouseReleaseEvent(self, event):
-        self.setPos(self.parent.boundingRect().bottomRight())
-        super(ResizerItem, self).mouseReleaseEvent(event)
+    # def mouseReleaseEvent(self, event):
+    #     self.setPos(self.parent.boundingRect().bottomRight())
+    #     super(ResizerItem, self).mouseReleaseEvent(event)
+    #
+    # def dragMoveEvent(self, event):
+    #     self.setPos(event.pos().x(), event.pos().x())
+    #     print("sdfhs")
+    #     super(ResizerItem, self).dragMoveEvent(event)
