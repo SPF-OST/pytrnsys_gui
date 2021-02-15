0788b8e5fae0a8c0eb9ddf2bff2bd3893964dcdd
Damian Birchler <damian.birchler@ost.ch>
2021-02-15 14:24:29 +0100

    Add command line flag to enable tracing.

---
+++ trnsysGUI/tracing.py
@@ -0,0 +1,38 @@
+import trace as _trc
+import typing as _tp
+import abc as _abc
+import logging as _log
+
+_TCo = _tp.TypeVar('_TCo')
+
+__all__ = ['createTracer', 'TracerBase']
+
+_logger = _log.getLogger('root')
+
+
+class TracerBase(_abc.ABC):
+    @_abc.abstractmethod
+    def run(self, func: _tp.Callable[[], _TCo]) -> _TCo:
+        pass
+
+
+class _DummyTracer(TracerBase):
+    def run(self, func: _tp.Callable[[], _TCo]) -> _TCo:
+        return func()
+
+
+class _Tracer(TracerBase):
+    def __init__(self):
+        self._trace = _trc.Trace(count=False, trace=True, timing=True)
+
+    def run(self, func: _tp.Callable[[], _TCo]) -> _TCo:
+        return self._trace.runfunc(func)
+
+
+def createTracer(shallTrace: bool) -> TracerBase:
+    if not shallTrace:
+        _logger.debug('Tracing is DISABLED.')
+        return _DummyTracer()
+
+    _logger.info('Tracing is ENABLED.')
+    return _Tracer()
