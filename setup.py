77cb8a88c09f7115b390d35a311c4c4bcb77ae17
Damian Birchler <damian.birchler@ost.ch>
2021-04-09 12:59:52 +0200

    Add dataclasses_jsonschema as requirement.

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setuptools.setup(
         "numpy",
         "pandas",
         "bokeh",
+        "dataclasses_jsonschema",
         "appdirs",
     ],
     setup_requires=["setuptools-git-versioning"],
