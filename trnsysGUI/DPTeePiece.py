4d7ff7cbb31b24e5cec27a6f0fed16b502bfbc1d
Simon Schläpfer <simon.schlaepfer@ost.ch>
2021-10-27 13:12:32 +0200

    updated DPTeePiece to pass pylint tests

--- trnsysGUI/DPTeePiece.py
+++ trnsysGUI/DPTeePiece.py
@@ -1,12 +1,9 @@
-# pylint: skip-file
-# type: ignore
-
 import typing as _tp
 
 import trnsysGUI.images as _img
 from massFlowSolver import InternalPiping
-from trnsysGUI.BlockItem import BlockItem
-from trnsysGUI.DoublePipePortItem import DoublePipePortItem
+from trnsysGUI.BlockItem import BlockItem # type: ignore[attr-defined]
+from trnsysGUI.DoublePipePortItem import DoublePipePortItem # type: ignore[attr-defined]
 
 
 class DPTeePiece(BlockItem):
@@ -35,23 +32,7 @@ class DPTeePiece(BlockItem):
             return _img.DP_TEE_PIECE_ROTATED_270
 
     def changeSize(self):
-        w = self.w
-        h = self.h
-
-        # Limit the block size:
-        if h < 20:
-            h = 20
-        if w < 40:
-            w = 40
-
-        # center label:
-        rect = self.label.boundingRect()
-        lw, lh = rect.width(), rect.height()
-        lx = (w - lw) / 2
-
-        self.label.setPos(lx, h - self.flippedV * (h + h / 2))
-
-        self.origInputsPos = [[0, 30], [w, 30]]
+        self.origInputsPos = [[0, 30], [40, 30]]
         self.origOutputsPos = [[30, 0]]
         self.inputs[0].setPos(self.origInputsPos[0][0], self.origInputsPos[0][1])
         self.inputs[1].setPos(self.origInputsPos[1][0], self.origInputsPos[1][1])
@@ -64,67 +45,11 @@ class DPTeePiece(BlockItem):
         self.inputs[1].side = (self.rotationN + 2 - 2 * self.flippedH) % 4
         self.outputs[0].side = (self.rotationN + 1 - 1 * self.flippedH) % 4
 
-        return w, h
-
     def getInternalPiping(self) -> InternalPiping:
-        teePiece, modelPortItemsToGraphicalPortItem = self._getModelAndMapping()
-
-        internalPiping = InternalPiping([teePiece], modelPortItemsToGraphicalPortItem)
-
-        return internalPiping
+        pass
 
     def _getModelAndMapping(self):
-        input1 = _mfn.PortItem()
-        input2 = _mfn.PortItem()
-        output = _mfn.PortItem()
-        teePiece = _mfn.TeePiece(self.displayName, self.trnsysId, input1, input2, output)
-        modelPortItemsToGraphicalPortItem = {input1: self.inputs[0], input2: self.inputs[1], output: self.outputs[0]}
-        return teePiece, modelPortItemsToGraphicalPortItem
+        pass
 
     def exportPipeAndTeeTypesForTemp(self, startingUnit):
-        if self.isVisible():
-            f = ""
-            unitNumber = startingUnit
-            tNr = 929  # Temperature calculation from a tee-piece
-
-            unitText = ""
-            ambientT = 20
-
-            equationConstant = 1
-
-            unitText += "UNIT " + str(unitNumber) + " TYPE " + str(tNr) + "\n"
-            unitText += "!" + self.displayName + "\n"
-            unitText += "PARAMETERS 0\n"
-            unitText += "INPUTS 6\n"
-
-            openLoops, nodesToIndices = self._getOpenLoopsAndNodeToIndices()
-            assert len(openLoops) == 1
-            openLoop = openLoops[0]
-
-            realNodes = [n for n in openLoop.nodes if isinstance(n, _mfn.RealNodeBase)]
-            assert len(realNodes) == 1
-            realNode = realNodes[0]
-
-            outputVariables = realNode.serialize(nodesToIndices).outputVariables
-            for outputVariable in outputVariables:
-                if not outputVariable:
-                    continue
-
-                unitText += outputVariable.name + "\n"
-
-            unitText += f"T{self.inputs[0].connectionList[0].displayName}\n"
-            unitText += f"T{self.inputs[1].connectionList[0].displayName}\n"
-            unitText += f"T{self.outputs[0].connectionList[0].displayName}\n"
-
-            unitText += "***Initial values\n"
-            unitText += 3 * "0 " + 3 * (str(ambientT) + " ") + "\n"
-
-            unitText += "EQUATIONS 1\n"
-            unitText += "T" + self.displayName + "= [" + str(unitNumber) + "," + str(equationConstant) + "]\n"
-
-            unitNumber += 1
-            f += unitText + "\n"
-
-            return f, unitNumber
-        else:
-            return "", startingUnit
+        pass
