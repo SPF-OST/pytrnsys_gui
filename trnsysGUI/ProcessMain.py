928957f1837ed5a9d4a59454fea0506ca900c11b
Martin Neugebauer <martin.neugebauer@ost.ch>
2021-11-19 14:02:50 +0100

    Removed deprecated input from ProcessMain.py

--- trnsysGUI/ProcessMain.py
+++ trnsysGUI/ProcessMain.py
@@ -4,7 +4,6 @@
 import sys
 
 sys.path.append("C:\\Users\\parad\\OneDrive\\Desktop\\pytrnsys\\pytrnsys")
-import pytrnsys.rsim.runParallelTrnsys as runTrnsys
 import pytrnsys.psim.processParallelTrnsys as processTrnsys
 
 import os
