84c729e3975e178d845acce4bda462167995b5bf
Martin Neugebauer <martin.neugebauer@ost.ch>
2022-01-28 12:44:39 +0100

    Added missing arguments to Connector class

--- trnsysGUI/Connector.py
+++ trnsysGUI/Connector.py
@@ -11,8 +11,8 @@ from trnsysGUI.massFlowSolver.networkModel import Pipe, PortItem, PortItemType
 
 
 class Connector(BlockItem, MassFlowNetworkContributorMixin):
-    def __init__(self, trnsysType, parent):
-        super().__init__(trnsysType, parent)
+    def __init__(self, trnsysType, parent, **kwargs):
+        super(Connector, self).__init__(trnsysType, parent, **kwargs)
         self.sizeFactor = 0.5
         self.w = 40
         self.h = 40
