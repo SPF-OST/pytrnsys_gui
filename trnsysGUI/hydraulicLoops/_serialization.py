d2d81ba9e9a469497d0c1b252dfa938aa0289289
Damian Birchler <damian.birchler@ost.ch>
2022-03-14 23:29:59 +0100

    Use previous version in `getSupersededClass`, not self.

--- trnsysGUI/hydraulicLoops/_serialization.py
+++ trnsysGUI/hydraulicLoops/_serialization.py
@@ -48,7 +48,7 @@ class HydraulicLoop(_ser.UpgradableJsonSchemaMixin):
 
     @classmethod
     def upgrade(cls, superseded: UpgradableJsonSchemaMixinVersion0) -> "HydraulicLoop":
-        assert isinstance(superseded, HydraulicLoop)
+        assert isinstance(superseded, HydraulicLoopVersion0)
 
         useLoopWideDefaults = False
 
