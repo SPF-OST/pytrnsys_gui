bf080028a44b3ec4154c2d53205b026fe0a7fba8
ahobeost <ahobeost@github.com>
2023-01-18 19:39:18 +0100

    Expanded the test for acceptable and unacceptable segmentdlg names

--- tests/trnsysGUI/testSegmentDlg.py
+++ tests/trnsysGUI/testSegmentDlg.py
@@ -3,9 +3,28 @@ import pytest as _pt
 from trnsysGUI.segmentDlg import NameContainsUnacceptableCharacters
 
 
-def testNameContainsUnacceptableCharacters():
-    userInput = " name with spaces "
+@_pt.mark.parametrize("userInput", [
+    " name with spaces ",
+    "_name_with_underscores_",
+    "name%with^strange$characters+",
+])
+def testNameContainsUnacceptableCharacters(userInput):
     response = NameContainsUnacceptableCharacters(userInput)
 
-    assert response == True
+    assert response is True
 
+
+@_pt.mark.parametrize("userInput", [
+    "nameisnoncapitalwithonlyletters",
+    "1337",
+    "NameIsCamelCaseWithOnlyLetters",
+    "name1contains2numbers3without4capital5letters",
+    "name1contains2numbers3with4CAPITAL5LETTERS",
+])
+def testNameContainsOnlyAcceptableCharacters(userInput):
+    response = NameContainsUnacceptableCharacters(userInput)
+
+    assert response is False
+
+    # response = "Found unacceptable characters (this includes spaces at the start and the end)\n" \
+    #                "Please use only letters and numbers. "
