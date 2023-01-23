89762e724ecfe87b778c37c7f777cfcec2d100ca
ahobeost <ahobeost@github.com>
2023-01-23 12:45:04 +0100

    Adjusted test to actual usage

--- tests/trnsysGUI/testSegmentDlg.py
+++ tests/trnsysGUI/testSegmentDlg.py
@@ -1,23 +1,24 @@
 import pytest as _pt
 
-from trnsysGUI.segmentDlg import respondToUnacceptableNaming
+import trnsysGUI.segmentDlg as _sd
 
-unacceptableNames = [" name with spaces ", "_name_with_underscores_", "name%with^strange$characters+"]
-acceptableNames = ["nameisnoncapitalwithonlyletters", "1337", "NameIsCamelCaseWithOnlyLetters",
-                   "name1contains2numbers3without4capital5letters", "name1contains2numbers3with4CAPITAL5LETTERS"]
+unacceptableNames = [" name with spaces ", "name%with^strange$characters+", "1337"]
+acceptableNames = ["nameisnoncapitalwithonlyletters",  "NameIsCamelCaseWithOnlyLetters",
+                   "name1contains2numbers3without4capital5letters", "name1contains2numbers3with4CAPITAL5LETTERS",
+                   "_name_with_underscores_"]
 
 
 # todo: apply these tests to the class segmentDlg
 @_pt.mark.parametrize("userInput", unacceptableNames)
 def testNameContainsUnacceptableCharacters(userInput):
-    responseClass = respondToUnacceptableNaming(userInput)
+    responseClass = _sd.respondToUnacceptableNaming(userInput)
 
     assert responseClass.unAcceptableName is True
 
 
 @_pt.mark.parametrize("userInput", acceptableNames)
 def testNameContainsOnlyAcceptableCharacters(userInput):
-    responseClass = respondToUnacceptableNaming(userInput)
+    responseClass = _sd.respondToUnacceptableNaming(userInput)
 
     assert responseClass.unAcceptableName is False
 
@@ -26,6 +27,6 @@ def testNameContainsOnlyAcceptableCharacters(userInput):
 def testResponseToUnacceptableName(userInput):
     expected_response = "Found unacceptable characters (this includes spaces at the start and the end)\n" \
                         "Please use only letters and numbers."
-    responseClass = respondToUnacceptableNaming(userInput)
+    responseClass = _sd.respondToUnacceptableNaming(userInput)
     print(responseClass.response)
     assert responseClass.response == expected_response
