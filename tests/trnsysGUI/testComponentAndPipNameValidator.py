7a1b4467bed8013a7b20d2a6c4d14bc1f8099de8
Damian Birchler <damian.birchler@ost.ch>
2023-02-20 15:45:28 +0100

    Revert "Actually test whether the error message matches the expected one."

--- tests/trnsysGUI/testComponentAndPipNameValidator.py
+++ tests/trnsysGUI/testComponentAndPipNameValidator.py
@@ -1,6 +1,5 @@
 import pytest as _pt
 
-import pytrnsys.utils.result as _res
 import trnsysGUI.componentAndPipeNameValidator as _cpn
 
 _EXISTING_NAMES = ["SCnrA_QSnkA", "SCnrB_QSnkB", "SCnrC_QSnkC", "SCnrD_QSnkD"]
@@ -15,8 +14,6 @@ _INVALID_CHARACTER_ERROR_MESSAGE = (
     "Found unacceptable characters (this includes spaces at the start and the end)\n"
     "Please use only letters, numbers, and underscores."
 )
-_NAME_ALREADY_EXISTS_ERROR_MESSAGE = "Name already exist (note: names are case insensitive)."
-
 _NAMES_AND_ERROR_MESSAGES = [
     (" name with spaces ", _INVALID_CHARACTER_ERROR_MESSAGE),
     ("name%with^strange$characters+", _INVALID_CHARACTER_ERROR_MESSAGE),
@@ -29,27 +26,31 @@ _NAMES_AND_ERROR_MESSAGES = [
         _INVALID_CHARACTER_ERROR_MESSAGE,
     ),
     ("", "Please enter a name."),
-    ("SCnrA_QSnkA", _NAME_ALREADY_EXISTS_ERROR_MESSAGE),
-    ("SCnra_QSnkA", _NAME_ALREADY_EXISTS_ERROR_MESSAGE),
+    ("SCnrA_QSnkA", "Name already exist (note: names are case insensitive)."),
+    ("SCnra_QSnkA", "Name already exist (note: names are case insensitive).")
 ]
 
 _INVALID_NAMES = [n for n, _ in _NAMES_AND_ERROR_MESSAGES]
 
 
 class TestComponentAndPipeNameValidator:
-    @_pt.mark.parametrize(["newName", "expectedErrorMessage"], _NAMES_AND_ERROR_MESSAGES)
-    def testValidateNameInvalidNames(self, newName, expectedErrorMessage):
+    @_pt.mark.parametrize("newName", _INVALID_NAMES)
+    def testValidateNameInvalidNames(self, newName):
         validator = _cpn.ComponentAndPipeNameValidator(_EXISTING_NAMES)
-        result = validator.validateName(newName)
+        errorMessage = validator.validateName(newName)
 
-        assert _res.isError(result)
-
-        errorMessage = _res.error(result).message
-        assert errorMessage == expectedErrorMessage
+        assert errorMessage
 
     @_pt.mark.parametrize("newName", _VALID_NAMES)
     def testValidateNameValidNames(self, newName):
         validator = _cpn.ComponentAndPipeNameValidator(_EXISTING_NAMES)
-        result = validator.validateName(newName)
+        errorMessage = validator.validateName(newName)
 
-        assert not _res.isError(result)
+        assert not errorMessage
+
+    @_pt.mark.parametrize("newName, expectedErrorMessage", _NAMES_AND_ERROR_MESSAGES)
+    def testResponseToUnacceptableName(self, newName, expectedErrorMessage):
+        validator = _cpn.ComponentAndPipeNameValidator(_EXISTING_NAMES)
+        errorMessage = validator.validateName(newName)
+
+        assert errorMessage == expectedErrorMessage
