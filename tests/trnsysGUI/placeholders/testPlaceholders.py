356cdcf09eecc8ecc492222f85bab66630c9735e
Damian Birchler <damian.birchler@ost.ch>
2023-03-16 14:57:13 +0100

    Don't use finalizers to `QApplication.close` but `qtbot`.
    See: https://github.com/pytest-dev/pytest-qt/issues/482.

--- tests/trnsysGUI/placeholders/testPlaceholders.py
+++ tests/trnsysGUI/placeholders/testPlaceholders.py
@@ -1,9 +1,6 @@
 import logging as _log
 import pathlib as _pl
 
-import PyQt5.QtWidgets as _qtw
-import pytest as _pt
-
 import pytrnsys.utils.result as _res
 import trnsysGUI.diagram.Editor as _de
 
@@ -11,21 +8,15 @@ _DATA_DIR_ = _pl.Path(__file__).parent / "data"
 
 
 class TestPlaceholders:
-    def testPlaceholders(self, request: _pt.FixtureRequest):
+    def testPlaceholders(self, qtbot):
         actualDirPath = _DATA_DIR_ / "TRIHP_dualSource"
         expectedDirPath = _DATA_DIR_ / "expected"
         actualJsonFilePath = actualDirPath / "DdckPlaceHolderValues.json"
         expectedJsonFilePath = expectedDirPath / "DdckPlaceHolderValues.json"
 
-        # The following line is required otherwise QT will crash
-        application = _qtw.QApplication([])
-
-        def quitApplication():
-            application.quit()
-
-        request.addfinalizer(quitApplication)
-
         editor = self._createEditor(actualDirPath)
+        qtbot.addWidget(editor)
+
         result = editor.encodeDdckPlaceHolderValuesToJson(actualJsonFilePath)
         assert not _res.isError(result)
 
