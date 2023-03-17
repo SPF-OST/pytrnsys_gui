30b545be78be03af1ec788325b3001170b52524e
Damian Birchler <damian.birchler@ost.ch>
2023-03-17 15:07:40 +0100

    Don't pass hard-coded strings to `unittest.mock.patch`.
    Also, rename some classes, etc.

--- tests/trnsysGUI/dialogs/connectionDialogs/testDoublePipeConnectionLengthDialog.py
+++ tests/trnsysGUI/dialogs/connectionDialogs/testDoublePipeConnectionLengthDialog.py
@@ -1,42 +1,46 @@
 import unittest.mock as _m
 
-from PyQt5 import QtCore
+import PyQt5.QtCore as _qtc
 
 import trnsysGUI.dialogs.connectionDialogs.doublePipeConnectionLengthDialog as _dpcdlg
+import trnsysGUI.errors as _errors
+
+
+_SHOW_ERROR_FUNCTION_NAME = f"{_errors.__name__}.{_errors.showErrorMessageBox.__name__}"
 
 
 class TestDoublePipeConnectionLengthDialog:  # pylint: disable = attribute-defined-outside-init
     def setup(self):
-        self.dPConnection = _dpcdlg.DPConnection(5)
-        self.widget = _dpcdlg.DoublePipeConnectionLengthDialog(self.dPConnection)
+        self.connectionModel = _dpcdlg.ConnectionModel(5)
+        self.dialog = _dpcdlg.DoublePipeConnectionLengthDialog(self.connectionModel)
 
     def _testHelper(self, bot, value, button="okButton"):
-        bot.addWidget(self.widget)
+        bot.addWidget(self.dialog)
         self._clearAndWriteAndPressButton(bot, value, button)
 
     def _clearAndWriteAndPressButton(self, bot, value, button="okButton"):
-        self.widget.lineEdit.clear()
-        bot.keyClicks(self.widget.lineEdit, value)
-        bot.mouseClick(getattr(self.widget, button), QtCore.Qt.LeftButton)
+        self.dialog.lineEdit.clear()
+        bot.keyClicks(self.dialog.lineEdit, value)
+        bot.mouseClick(getattr(self.dialog, button), _qtc.Qt.LeftButton)
 
     def testDialogLineEdit(self, qtbot):
         self._testHelper(qtbot, "7")
-        assert self.dPConnection.lengthInM == 7
+        assert self.connectionModel.lengthInM == 7
 
     def testDialogCancel(self, qtbot):
         self._testHelper(qtbot, "7", button="cancelButton")
-        assert self.dPConnection.lengthInM == 5
+        assert self.connectionModel.lengthInM == 5
 
     def testDialogRaises(self, qtbot):
-        with _m.patch("trnsysGUI.errors.showErrorMessageBox") as box:
+        with _m.patch(_SHOW_ERROR_FUNCTION_NAME) as box:
             self._testHelper(qtbot, "-7")
             box.assert_called_with(errorMessage="Value must be positive.", title="Almost there")
 
     def testDialogInputAfterRaises(self, qtbot):
-        with _m.patch("trnsysGUI.errors.showErrorMessageBox"):
+        with _m.patch(_SHOW_ERROR_FUNCTION_NAME):
             self._testHelper(qtbot, "-7")
             self._clearAndWriteAndPressButton(qtbot, "9")
-            assert self.dPConnection.lengthInM == 9
+            assert self.connectionModel.lengthInM == 9
 
     def teardown(self):
-        self.widget.close()
+        self.dialog.close()
