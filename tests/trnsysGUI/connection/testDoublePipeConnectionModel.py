77bccb5691beb59c815a1dbee0a203c86c162ce8
Damian Birchler <damian.birchler@ost.ch>
2023-03-17 18:14:50 +0100

    Test that default length is applied on upgrade, and on upgrade only.

--- tests/trnsysGUI/connection/testDoublePipeConnectionModel.py
+++ tests/trnsysGUI/connection/testDoublePipeConnectionModel.py
@@ -1,23 +1,8 @@
-import uuid as _uuid
 import pytest as _pt
 
 import trnsysGUI.connection.doublePipeConnectionModel as _dpcm
 
 _JSON_VERSION_0 = {
-    "__version__": "0810c9ea-85df-4431-bb40-3190c25c9161",
-    "childIds": [2, 3, 4],
-    "connectionId": 1,
-    "fromPortId": 17,
-    "id": 1,
-    "labelPos": [1.1, 1.1],
-    "massFlowLabelPos": [1.1, 0],
-    "name": "dpc5",
-    "segmentsCorners": [[0, 0], [0, 1], [1, 0], [1, 1]],
-    "toPortId": 18,
-    "trnsysId": 500,
-}
-
-_JSON_VERSION_0_FROM = {
     ".__ConnectionDict__": True,
     "__version__": "0810c9ea-85df-4431-bb40-3190c25c9161",
     "childIds": [2, 3, 4],
@@ -40,7 +25,7 @@ _JSON_VERSION_1 = {
     "fromPortId": 17,
     "id": 1,
     "labelPos": [1.1, 1.1],
-    "lengthInM": 579.404,
+    "lengthInM": 2.0,
     "massFlowLabelPos": [1.1, 0],
     "name": "dpc5",
     "segmentsCorners": [[0, 0], [0, 1], [1, 0], [1, 1]],
@@ -48,20 +33,7 @@ _JSON_VERSION_1 = {
     "trnsysId": 500,
 }
 
-_MODEL0 = _dpcm.DoublePipeConnectionModelVersion0(
-    connectionId=1,
-    name="dpc5",
-    id=1,
-    childIds=[2, 3, 4],
-    segmentsCorners=[(0, 0), (0, 1), (1, 0), (1, 1)],
-    labelPos=(1.1, 1.1),
-    massFlowLabelPos=(1.1, 0),
-    fromPortId=17,
-    toPortId=18,
-    trnsysId=500,
-)
-
-_MODEL1 = _dpcm.DoublePipeConnectionModel(
+_UPGRADED_MODEL = _dpcm.DoublePipeConnectionModel(
     connectionId=1,
     name="dpc5",
     id=1,
@@ -75,18 +47,29 @@ _MODEL1 = _dpcm.DoublePipeConnectionModel(
     lengthInM=579.404,
 )
 
+_DESERIALIZED_MODEL = _dpcm.DoublePipeConnectionModel(
+    connectionId=1,
+    name="dpc5",
+    id=1,
+    childIds=[2, 3, 4],
+    segmentsCorners=[(0, 0), (0, 1), (1, 0), (1, 1)],
+    labelPos=(1.1, 1.1),
+    massFlowLabelPos=(1.1, 0),
+    fromPortId=17,
+    toPortId=18,
+    trnsysId=500,
+    lengthInM=2.0,
+)
+
 
 _TO_DICT_CASES = [
-    (_MODEL0, _JSON_VERSION_0),
-    (_MODEL1, _JSON_VERSION_1),
+    (_DESERIALIZED_MODEL, _JSON_VERSION_1),
 ]
 
 _FROM_DICT_CASES = [
-    (_dpcm.DoublePipeConnectionModelVersion0, _MODEL0, _JSON_VERSION_0_FROM),
-    (_dpcm.DoublePipeConnectionModel, _MODEL1, _JSON_VERSION_1),
+    (_dpcm.DoublePipeConnectionModel, _UPGRADED_MODEL, _JSON_VERSION_0),
+    (_dpcm.DoublePipeConnectionModel, _DESERIALIZED_MODEL, _JSON_VERSION_1),
 ]
-
-_VERSION_CASES = [(x, z) for x, y, z in _FROM_DICT_CASES]
 
 
 class TestDoublePipeConnectionModel:
@@ -97,16 +80,12 @@ class TestDoublePipeConnectionModel:
 
     @_pt.mark.parametrize("modelClass, model, json", _FROM_DICT_CASES)
     def testFromDict(self, modelClass, model, json):
-        result = modelClass.from_dict(json)
-        assert result == model
-
-    @_pt.mark.parametrize("modelClass, json", _VERSION_CASES)
-    def testGetVersion(self, modelClass, json):
-        version = modelClass.getVersion()
-        assert version == _uuid.UUID(json["__version__"])
+        deserializedModel = modelClass.from_dict(json)
+        assert deserializedModel == model
 
     def testGetSupersededClass(self):
         assert _dpcm.DoublePipeConnectionModel.getSupersededClass() == _dpcm.DoublePipeConnectionModelVersion0
 
     def testUpgrade(self):
-        assert _dpcm.DoublePipeConnectionModel.upgrade(_MODEL0) == _MODEL1
+        version0 = _dpcm.DoublePipeConnectionModelVersion0.from_dict(_JSON_VERSION_0)
+        assert _dpcm.DoublePipeConnectionModel.upgrade(version0) == _UPGRADED_MODEL
