cd69d7b28ca4922f813947011abd33517b4aa344
Damian Birchler <damian.birchler@ost.ch>
2023-04-19 15:11:25 +0200

    Use `git` to delete untracked output files between test runs.

--- tests/trnsysGUI/diagram/testEditor.py
+++ tests/trnsysGUI/diagram/testEditor.py
@@ -264,17 +264,91 @@ class _Helper:
         if self._project.exampleDirNameToCopyFrom:
             self._copyExampleToTestInputFolder()
 
-        self._removeGeneratedFiles()
+        self._removeUntrackedPaths()
 
-    def _removeGeneratedFiles(self):
-        for child in self.actualProjectFolderPath.iterdir():
-            if child.name in ("ddck", f"{self._project.projectName}.json", "run.config"):
+    def _removeUntrackedPaths(self):
+        untrackedPaths = self._getUntrackedPathsInActualProjectFolder()
+
+        for untrackedPath in untrackedPaths:
+            if untrackedPath.is_dir():
+                _su.rmtree(untrackedPath)
+            else:
+                untrackedPath.unlink()
+
+    def _getUntrackedPathsInActualProjectFolder(self):
+        projectFolderPathUnderVersionControl = self._getProjectFolderPathUnderVersionControl()
+
+        untrackedRelativePaths = self._getUntrackedRelativePaths(projectFolderPathUnderVersionControl)
+
+        untrackedAbsolutePaths = [self.actualProjectFolderPath / p for p in untrackedRelativePaths]
+
+        return untrackedAbsolutePaths
+
+    def _getProjectFolderPathUnderVersionControl(self):
+        if self._project.exampleDirNameToCopyFrom:
+            return self._getProjectFolderPathInExamplesDir()
+
+        return self.actualProjectFolderPath
+
+    def _getProjectFolderPathInExamplesDir(self):
+        assert self._project.exampleDirNameToCopyFrom
+
+        pytrnsysGuiDir = _pl.Path(__file__).parents[3]
+
+        exampleProjectPath = (
+            pytrnsysGuiDir / "data" / self._project.exampleDirNameToCopyFrom / self._project.projectName
+        )
+
+        return exampleProjectPath
+
+    def _getUntrackedRelativePaths(self, projectFolderPathUnderVersionControl):
+        untrackedPathsRelativeToRepoRoot = self._getUntrackedPathsRelativeToRepoRoot(
+            projectFolderPathUnderVersionControl
+        )
+
+        repositoryRootDirPath = self._getRepositoryRootDirPath()
+
+        untrackedAbsolutePaths = [repositoryRootDirPath / p for p in untrackedPathsRelativeToRepoRoot]
+
+        untrackedRelativePaths = [p.relative_to(projectFolderPathUnderVersionControl) for p in untrackedAbsolutePaths]
+
+        return untrackedRelativePaths
+
+    @staticmethod
+    def _getUntrackedPathsRelativeToRepoRoot(projectFolderPathUnderVersionControl):
+        completedProcess = _sp.run(
+            ["git", "status", "-unormal", "--porcelain", str(projectFolderPathUnderVersionControl)],
+            check=True,
+            text=True,
+            capture_output=True,
+        )
+
+        statusesAndUntrackedPath = completedProcess.stdout.splitlines()
+
+        untrackedPaths = []
+        for statusAndUntrackedPath in statusesAndUntrackedPath:
+            status, untrackedPath = statusAndUntrackedPath.split()
+
+            modifiedStatus = "??"
+            if status != modifiedStatus:
                 continue
 
-            if child.is_dir():
-                _su.rmtree(child)
-            else:
-                child.unlink()
+            untrackedPaths.append(untrackedPath)
+
+        return untrackedPaths
+
+    @staticmethod
+    def _getRepositoryRootDirPath():
+        completedProcess = _sp.run(
+            ["git", "rev-parse", "--show-toplevel"],
+            check=True,
+            text=True,
+            capture_output=True,
+        )
+
+        repositoryRootDirPath = completedProcess.stdout.splitlines()[0]
+
+        return _pl.Path(repositoryRootDirPath)
 
     def ensureFilesAreEqual(
         self, fileRelativePathAsString: str, replaceInExpected: _tp.Optional[_tp.Tuple[str, str]] = None
@@ -317,7 +391,6 @@ class _Helper:
         if self.actualProjectFolderPath.exists():
             _su.rmtree(self.actualProjectFolderPath)
 
-        pytrnsysGuiDir = _pl.Path(__file__).parents[3]
-        exampleFolderPath = pytrnsysGuiDir / "data" / self._project.exampleDirNameToCopyFrom / self._project.projectName
+        projectPathInExamplesDir = self._getProjectFolderPathInExamplesDir()
 
-        _su.copytree(exampleFolderPath, self.actualProjectFolderPath)
+        _su.copytree(projectPathInExamplesDir, self.actualProjectFolderPath)
