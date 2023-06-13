094258fe65c505b01bd2151059f59a1d9865892a
ahobeost <ahobeost@github.com>
2023-06-13 11:36:15 +0200

    Added simple json case with two working regimes and one regime that throws an error.

--- tests/trnsysGUI/pythonInterface/regimeExporter/testGetDesiredRegimes.py
+++ tests/trnsysGUI/pythonInterface/regimeExporter/testGetDesiredRegimes.py
@@ -8,17 +8,17 @@ _DATA_DIR_ = _pl.Path(_GUI.__file__).parent / "..\\tests\\trnsysGUI\\data"
 
 
 class TestGetDesiredRegimes:
-    def testRegimes(self):
+    def testgetRegimesFromFile(self):
         fileName = _DATA_DIR_ / "regimes.csv"
         df1 = _gdr.getRegimesFromFile(fileName)
         df2 = _pd.DataFrame(
             {
-                "regimeNames": ["name1", "name2"],
-                "pump1": [500, 0],
-                "pump2": [0, 500],
-                "pump3": [0, 0],
-                "valve1": [0, 0],
-                "valve2": [0, 1],
+                "regimeNames": ["name1", "name2", "throws"],
+                "pump1": [500, 500, 0],
+                "pump2": [0, 500, 0],
+                "pump3": [500, 0, 500],
+                "valve1": [0, 0, 0],
+                "valve2": [0, 1, 1],
             }
         )
         assert df1.equals(df2)
