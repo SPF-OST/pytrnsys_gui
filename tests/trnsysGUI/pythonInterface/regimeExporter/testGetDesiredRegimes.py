fb2e8c7c3c0fa97f1de2f18f7776b19c5557572f
ahobeost <ahobeost@github.com>
2023-06-13 13:06:48 +0200

    added error handling for column name

--- tests/trnsysGUI/pythonInterface/regimeExporter/testGetDesiredRegimes.py
+++ tests/trnsysGUI/pythonInterface/regimeExporter/testGetDesiredRegimes.py
@@ -1,19 +1,22 @@
 import pandas as _pd
 import pathlib as _pl
+import pytest as _pt
 
 import trnsysGUI.pythonInterface.regimeExporter.getDesiredRegimes as _gdr
 import trnsysGUI as _GUI
 
 _DATA_DIR_ = _pl.Path(_GUI.__file__).parent / "..\\tests\\trnsysGUI\\data\\diagramForRegimes"
 
+_COL_NAME_ = "regimeName"
+
 
 class TestGetDesiredRegimes:
-    def testgetRegimesFromFile(self):
+    def testGetRegimesFromFile(self):
         fileName = _DATA_DIR_ / "regimes.csv"
         df1 = _gdr.getRegimesFromFile(fileName)
         df2 = _pd.DataFrame(
             {
-                "regimeNames": ["name1", "name2", "throws"],
+                _COL_NAME_: ["name1", "name2", "throws"],
                 "pump1": [500, 500, 0],
                 "pump2": [0, 500, 0],
                 "pump3": [500, 0, 500],
@@ -22,3 +25,9 @@ class TestGetDesiredRegimes:
             }
         )
         assert df1.equals(df2)
+
+    def testColumnNameIncorrectRaises(self):
+        fileName = _DATA_DIR_ / "regimes_incorrect.csv"
+        with _pt.raises(ValueError, match=f"Column name '{_COL_NAME_}' not found."):
+            _gdr.getRegimesFromFile(fileName)
+
--- trnsysGUI/pythonInterface/regimeExporter/getDesiredRegimes.py
+++ trnsysGUI/pythonInterface/regimeExporter/getDesiredRegimes.py
@@ -1,11 +1,17 @@
 import pandas as _pd
 
+# dealing with files where the regimeName column is not provided correctly
+
 
 def getRegimesFromFile(fileName):
     df = _pd.read_csv(fileName)
-    return df
+    _COL_NAME_ = "regimeName"
+    if _COL_NAME_ in df.keys():
+        return df
+    raise ValueError(f"Column name '{_COL_NAME_}' not found.")
 
-
+# error handling?
+#   - will throw "fileNotFoundError" as it is.
 #
 #
 # regimeName, name1, name2, name3
