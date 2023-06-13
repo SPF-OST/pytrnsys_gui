5661464bffa2a1252b758fd2139b1c62d7bf6a59
ahobeost <ahobeost@github.com>
2023-06-13 14:06:46 +0200

    Working version of both the massFlow solver and the pdf creation

---
+++ tests/trnsysGUI/pythonInterface/regimeExporter/testPrintRegimesAndCopyFiles.py
@@ -0,0 +1,37 @@
+import pathlib as _pl
+
+import pytrnsys.utils.log as _ulog
+
+import trnsysGUI as _GUI
+import trnsysGUI.mainWindow as _mw
+import trnsysGUI.project as _prj
+import trnsysGUI.pythonInterface.regimeExporter.renderDiagramOnPDFfromPython as _rdopfp
+
+_PROJECT_NAME_ = "diagramForRegimes"
+_DATA_DIR_ = _pl.Path(_GUI.__file__).parent / "..\\tests\\trnsysGUI\\data\\diagramForRegimes"
+_DATA_FILENAME_ = "regimes.csv"
+
+
+def _createMainWindow(PROJECT_FOLDER, PROJECT_NAME, qtbot):
+    projectJsonFilePath = PROJECT_FOLDER / f"{PROJECT_NAME}.json"
+    project = _prj.LoadProject(projectJsonFilePath)
+
+    logger = _ulog.getOrCreateCustomLogger("root", "DEBUG")  # type: ignore[attr-defined]
+
+    mainWindow = _mw.MainWindow(logger, project)  # type: ignore[attr-defined]
+
+    qtbot.addWidget(mainWindow)
+    mainWindow.showBoxOnClose = False
+
+    return mainWindow
+
+
+class TestPrintRegimesAndCopyFiles:
+    def testUsingQtBot(self, qtbot):
+        mainWindow = _createMainWindow(_DATA_DIR_, _PROJECT_NAME_, qtbot)
+        _rdopfp.printRegimesAndCopyFiles(_DATA_DIR_, _PROJECT_NAME_, _DATA_FILENAME_, mainWindow)
+        assert True is False
+
+# massFlowSolver should raise error when issue encountered.
+# rest of workflow should be ignored for this regime, instead a warning should be issued
+# non-qtbot solution?
--- trnsysGUI/pythonInterface/regimeExporter/renderDiagramOnPDFfromPython.py
+++ trnsysGUI/pythonInterface/regimeExporter/renderDiagramOnPDFfromPython.py
@@ -1,46 +1,46 @@
+import os as _os
+import pathlib as _pl
+import pytest as _pt
+import subprocess as _sp
+
 import PyQt5.QtGui as _qtg
 import PyQt5.QtPrintSupport as _qtp
 
 from tests.trnsysGUI.diagram import _testHelper as _th
 import trnsysGUI.project as _prj
+import trnsysGUI as _GUI
+import trnsysGUI.pythonInterface.regimeExporter.getDesiredRegimes as _gdr
+import trnsysGUI.mainWindow as _mw
+import trnsysGUI.diagram.Editor as _de
 
-
-def printDiagramToPDF(self, fileName):
-    if fileName != "":
-        printer = _qtp.QPrinter(_qtp.QPrinter.HighResolution)
-        printer.setOrientation(_qtp.QPrinter.Landscape)
-        printer.setOutputFormat(_qtp.QPrinter.PdfFormat)
-        printer.setOutputFileName(fileName)
-        painter = _qtg.QPainter(printer)
-        self.diagramScene.render(painter)
-        painter.end()
-        self.logger.info("File exported to %s" % fileName)
 
 # regimeValue = {regimeName: "", pumps: [], valves [] }
 # regimeValues = df[]
 # for regimeRow in df:
 
 
+def printRegimesAndCopyFiles(_DATA_DIR_, _PROJECT_NAME_, _DATA_FILENAME_, mainWindow):
+    regimeValues = _gdr.getRegimesFromFile(_DATA_DIR_ / _DATA_FILENAME_)
+    for ix in regimeValues.index:
+        regimeRow = regimeValues.loc[ix]
+        regimeName = regimeRow["regimeName"]
+        print(regimeName)
+        # adjustPumpsAndValves()
+        runMassFlowSolver(mainWindow)
+        # copyMFRandTfiles()
+        # adjustSlider(steps=2)
 
-PROJECT_NAME = "PCM_reverseable_airsource_HP_reduced"
-PROJECT_FOLDER = "."
-
-def printRegimesAndCopyFiles():
-    mainWindow = self._createMainWindow(PROJECT_FOLDER, PROJECT_NAME, qtbot, monkeypatch)
-
-    regimeValues = getRegimesValues()
-    for regimeRow in regimeValues.rows():
-        adjustPumpsAndValves()
-        runMassFlowSolver()
-        copyMFRandTfiles()
-        adjustSlider(steps=2)
-        printDiagramToPDF()
+        pdfName = str(_DATA_DIR_) + "\\" + _PROJECT_NAME_ + "_" + regimeName + ".pdf"
+        print(pdfName)
+        printDiagramToPDF(pdfName, mainWindow)
+        break
 
 
-
-def runMassFlowSolver(self, PROJECT_NAME, PROJECT_FOLDER, qtbot, monkeypatch) -> None:
-
-    self._exportMassFlowSolverDeckAndRunTrnsys(mainWindow.editor)
+def runMassFlowSolver(mainWindow) -> None:
+    try:
+        _exportMassFlowSolverDeckAndRunTrnsys(mainWindow.editor)
+    except Exception:
+        raise
 
     # massFlowSolverDeckFileName = f"{PROJECT_NAME}_mfs.dck"
 
@@ -52,23 +52,33 @@ def runMassFlowSolver(self, PROJECT_NAME, PROJECT_FOLDER, qtbot, monkeypatch) ->
     # printDiagramToPDF()
 
 
-def _createMainWindow(PROJECT_FOLDER, PROJECT_NAME, qtbot, monkeypatch):
-    def patchedCloseEvent(_, closeEvent):
-            return closeEvent.accept()
+def _exportMassFlowSolverDeckAndRunTrnsys(editor: _de.Editor):  # type: ignore[name-defined]
+    exportedFilePath = _exportHydraulic(editor, _format="mfs")
 
-    monkeypatch.setattr(
-        _mw.MainWindow,  # type: ignore[attr-defined]
-        _mw.MainWindow.closeEvent.__name__,  # type: ignore[attr-defined]
-        patchedCloseEvent,
-    )
+    trnExePath = _getTrnExePath()
 
-    projectJsonFilePath = PROJECT_FOLDER / f"{PROJECT_NAME}.json"
-    project = _prj.LoadProject(projectJsonFilePath)
+    _sp.run([str(trnExePath), str(exportedFilePath), "/H"], check=True)
 
-    logger = _ulog.getOrCreateCustomLogger("root", "DEBUG")  # type: ignore[attr-defined]
 
-    mainWindow = _mw.MainWindow(logger, project)  # type: ignore[attr-defined]
+def _exportHydraulic(editor: _de.Editor, *, _format) -> str:  # type: ignore[name-defined]
+    exportedFilePath = editor.exportHydraulics(exportTo=_format)
+    return exportedFilePath
 
-    qtbot.addWidget(mainWindow)
 
-    return mainWindow
+def _getTrnExePath():
+    isRunDuringCi = _os.environ.get("CI") == "true"
+    if isRunDuringCi:
+        return _pl.PureWindowsPath(r"C:\CI-Progams\TRNSYS18\Exe\TrnEXE.exe")
+
+    return _pl.PureWindowsPath(r"C:\TRNSYS18\Exe\TrnEXE.exe")
+
+
+def printDiagramToPDF(fileName, mainWindow):
+    if fileName != "":
+        printer = _qtp.QPrinter(_qtp.QPrinter.HighResolution)
+        printer.setOrientation(_qtp.QPrinter.Landscape)
+        printer.setOutputFormat(_qtp.QPrinter.PdfFormat)
+        printer.setOutputFileName(fileName)
+        painter = _qtg.QPainter(printer)
+        mainWindow.editor.diagramScene.render(painter)
+        painter.end()
