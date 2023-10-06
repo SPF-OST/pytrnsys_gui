c3678197cc9a24c951b40082f4ac2f6b3677da74
Damian Birchler <damian.birchler@ost.ch>
2023-10-06 13:49:39 +0200

    Don't allow non-existing paths.

--- release/src/runPytrnsys.py
+++ release/src/runPytrnsys.py
@@ -3,7 +3,7 @@ import pathlib as _pl
 import typer as _typer
 import typing_extensions as _te
 
-_ReadableFilePath = _te.Annotated[_pl.Path, _typer.Argument(readable=True, dir_okay=False)]
+_ReadableFilePath = _te.Annotated[_pl.Path, _typer.Argument(readable=True, dir_okay=False, exists=True)]
 
 
 def main(python_script_path: _ReadableFilePath) -> None:  # /NOSONAR  # pylint: disable=invalid-name
