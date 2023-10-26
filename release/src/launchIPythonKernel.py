0990ec7b3e374a171b8251cf26ad7d0ca8b712e3
Damian Birchler <damian.birchler@ost.ch>
2023-10-26 08:57:39 +0200

    Add `multiprocess` freeze support to `IPython` kernel launcher.

--- release/src/launchIPythonKernel.py
+++ release/src/launchIPythonKernel.py
@@ -1,15 +1,22 @@
 """
 Copied from
 https://github.com/ipython/ipykernel/blob/515004a331b10dff026b8d81a0571e4cdf1847a3/ipykernel_launcher.py
+with some modifications.
 """
 
-import sys
+import multiprocessing as _mp
+import sys as _sys
 
 if __name__ == "__main__":
+    # Support freezing using PyInstaller, see
+    # https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing and
+    # https://docs.python.org/3/library/multiprocessing.html#multiprocessing.freeze_support
+    _mp.freeze_support()
+
     # Remove the CWD from sys.path while we load stuff.
     # This is added back by InteractiveShellApp.init_path()
-    if sys.path[0] == "":
-        del sys.path[0]
+    if _sys.path[0] == "":
+        del _sys.path[0]
 
     from ipykernel import kernelapp as app
 
