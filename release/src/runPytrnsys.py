1630fee056545e78b92017c11c211a0cac50bc7e
Damian Birchler <damian.birchler@ost.ch>
2023-11-26 20:48:27 +0100

    Hacky way to pass on arguments to `process.py` & friends.

--- release/src/runPytrnsys.py
+++ release/src/runPytrnsys.py
@@ -1,5 +1,8 @@
 import multiprocessing as _mp
+import subprocess as _sp
+import sys as _sys
 import pathlib as _pl
+import typing as _tp
 
 import typer as _typer
 import typing_extensions as _te
@@ -7,14 +10,25 @@ import typing_extensions as _te
 _ReadableFilePath = _te.Annotated[_pl.Path, _typer.Argument(readable=True, dir_okay=False, exists=True)]
 
 
-def main(python_script_path: _ReadableFilePath) -> None:  # /NOSONAR  # pylint: disable=invalid-name
-    _runFile(python_script_path)
+def main(
+    python_script_path: _ReadableFilePath,
+    arguments_to_script: _te.Annotated[_tp.Optional[list[str]], _typer.Argument()] = None,
+) -> None:  # /NOSONAR  # pylint: disable=invalid-name
+    if arguments_to_script is None:
+        arguments_to_script = []
+
+    _runFile(python_script_path, arguments_to_script)
 
 
-def _runFile(pythonScriptPath: _pl.Path) -> None:
-    sourceCode = pythonScriptPath.read_text()
-    compiledCode = compile(sourceCode, pythonScriptPath, mode="exec")
-    exec(compiledCode)  # pylint: disable=exec-used
+def _runFile(pythonScriptPath: _pl.Path, argumentsToScript: _tp.Sequence[str]) -> None:
+    oldArgv = _sys.argv
+    try:
+        _sys.argv = [pythonScriptPath, *argumentsToScript]
+        sourceCode = pythonScriptPath.read_text()
+        compiledCode = compile(sourceCode, pythonScriptPath, mode="exec")
+        exec(compiledCode)  # pylint: disable=exec-use
+    finally:
+        _sys.argv = oldArgv
 
 
 if __name__ == "__main__":
