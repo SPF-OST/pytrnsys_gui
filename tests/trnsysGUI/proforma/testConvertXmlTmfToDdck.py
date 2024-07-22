f3903a797d96f93907e7abc034d9f24cdbb5a831
Damian Birchler <damian.birchler@ost.ch>
2024-07-22 13:09:57 +0200

    Integrate as is into code structure.

--- tests/trnsysGUI/proforma/testConvertXmlTmfToDdck.py
+++ tests/trnsysGUI/proforma/testConvertXmlTmfToDdck.py
@@ -1,35 +1,41 @@
 import pathlib as _pl
 import pprint as _pp
+import pkgutil as _pu
 
 import xmlschema as _xml
 
-import convertXmlTmfToDdck as _tmf
+import trnsysGUI.proforma.convertXmlTmfToDdck as _pc
 
 _CONTAINING_DIR_PATH = _pl.Path(__file__).parent
 
 
 def testValidateXmlTmf() -> None:
-    xsdFilePath = _CONTAINING_DIR_PATH / "xmltmf.xsd"
-    schema = _xml.XMLSchema11(xsdFilePath)
+    schema = _getSchema()
 
     xmlFilePath = _CONTAINING_DIR_PATH / "Type71.xmltmf"
     schema.validate(xmlFilePath)
 
 
 def testDecodeXmlTmf() -> None:
-    xsdFilePath = _CONTAINING_DIR_PATH / "xmltmf.xsd"
-    schema = _xml.XMLSchema11(xsdFilePath)
+    schema = _getSchema()
 
     xmlFilePath = _CONTAINING_DIR_PATH / "Type71.xmltmf"
     deserializedData = schema.to_dict(xmlFilePath)
     _pp.pprint(deserializedData, indent=4)
 
 
+def _getSchema() -> _xml.XMLSchema11:
+    data = _pu.get_data(_pc.__name__, "xmltmf.xsd")
+    string = data.decode("UTF8")
+    schema = _xml.XMLSchema11(string)
+    return schema
+
+
 def testConvertXmlTmfStringToDdck() -> None:
     xmlFilePath = _CONTAINING_DIR_PATH / "Type71.xmltmf"
     xmlFileContent = xmlFilePath.read_text(encoding="utf8")
 
-    actualDdckContent = _tmf.convertXmlTmfStringToDdck(xmlFileContent)
+    actualDdckContent = _pc.convertXmlTmfStringToDdck(xmlFileContent)
 
     expectedDdckFilePath = _CONTAINING_DIR_PATH / "Type71.ddck"
     expectedDdckContent = expectedDdckFilePath.read_text(encoding="utf8")
