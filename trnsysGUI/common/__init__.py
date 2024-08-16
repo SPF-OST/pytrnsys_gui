a902ed4190daf6f64501d4e6c24bed5104d095fc
Damian Birchler <damian.birchler@ost.ch>
2024-08-16 18:07:53 +0200

    Add `getOrAdd` method.

--- trnsysGUI/common/__init__.py
+++ trnsysGUI/common/__init__.py
@@ -1,6 +1,7 @@
 import typing as _tp
 
 _T = _tp.TypeVar("_T")
+_K = _tp.TypeVar("_K")
 
 
 def getSingle(iterable: _tp.Iterable[_T]) -> _T:
@@ -26,3 +27,14 @@ def getSingleOrNone(iterable: _tp.Iterable[_T]) -> _tp.Optional[_T]:
         raise ValueError("Expected one element but got more than one.")
 
     return values[0]
+
+
+def getOrAdd(key: _K, defaultValue: _T, data: dict[_K, _T]) -> _T:
+    value = data.get(key)
+
+    if value is not None:
+        return value
+
+    data[key] = defaultValue
+
+    return defaultValue
