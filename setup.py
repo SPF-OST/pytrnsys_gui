4e3c4b35d120c45b5b4ee3775d33ac1c0a99e70f
ahobeost <ahobeost@github.com>
2024-10-24 12:35:42 +0200

    Fixed pytrnsys not recognized by pycharm.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPL3 License",
         "Operating System :: OS Independent",
     ],
     package_data={
