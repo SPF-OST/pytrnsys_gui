37545332363e56d10358c63945a1a8fbb6cdbec5
Damian Birchler <damian@damianbirchler.net>
2025-01-29 11:49:13 +0100

    Revert "Don't consider `defaults.run.working-directory` when giving paths to actions."
    This reverts commit ccd40282061d6940fceb16de50a109eb2886a960.
    
    Revert "Remove working dir at beginning of release build workflow."
    
    This reverts commit c45a208a3a13f057f2199417ded2c1d05107b1a5.
    
    Revert "Use `working-directory` again."
    
    This reverts commit 49db7ec7f5c43e7cc780dd8df785796053d2e8e5.
    
    Revert "Use `-Force` when deleting non-empty dirs."
    
    This reverts commit d9da18df222632deb72186d1df937c987f7255b6.
    
    Revert "Don't use `working-directory` to find out why we can't delete working dirs."
    
    This reverts commit 377da1caddc9d6481f26596ac1cbc1e32cc59feb.
    
    Revert "Use `github.workspace` to specify root dir."
    
    This reverts commit 8c4ecac0426212db849f9c9eaed4d424955ee638.
    
    Revert "Remove stale working dirs at beginning."
    
    This reverts commit efd89e48a5a6c1ba08f8f31c3acbf368c20d77db.
    
    Revert "Can't use `env` at job level."
    
    This reverts commit 1f27d09ad8f63439568dc1ab9dd76d07fbabe9c8.
    
    Revert "Use `github.ref_name` instead of `github.ref`."
    
    This reverts commit 7d28cd78199b01ac686361363faef4c2e2c87d69.
    
    Revert "Correct syntax error."
    
    This reverts commit aad8777d20d400dd0d4b8326a88de8e16fe087ce.
    
    Revert "Correct script name."
    
    This reverts commit 6fab4ad952e39909ffd94fcc7be115457a0c7952.
    
    Revert "Satisfy static checkers."
    
    This reverts commit c942ab8deec895d98c58768e015a0f3f80f7b4a4.
    
    Revert "Provide string for 'client-payload'."
    
    This reverts commit 25f1636a9ba9df29e6d745beb0a4e7fcb4915e6c.
    
    Revert "Normal jobs needs 'runs-on'."
    
    This reverts commit f5e3dfabcd7c7b444bfae58f332465e99f191339.
    
    Revert "Actions must be run as step."
    
    This reverts commit c4ccd495ec2aa257b81f3baa086ba56f147aaca4.
    
    Revert "Install actual required versions when called as reusable workflow."
    
    This reverts commit 899398f3acf49f003e1a02d3cd5460f869e52d6d.
    
    Revert "Can only use reusable workflow for creating release for changes to `pytrnsys_gui`."
    
    This reverts commit 45f88dad3612fd9f5bb8429ce60b0537e8b0254f.
    
    Revert "Cant access `env` context at `jobs.<job-id>.if` level."
    
    This reverts commit 313b7e3483d93e1023b8dfa77b74f5e88bc2033a.
    
    Revert "Define `env`s at workflow level."
    
    This reverts commit 0beb975537016772b1aa92fa9fbf2ac5323827e6.
    
    Revert "Fix some bugs & whitespace."
    
    This reverts commit 37d41ed6d96a747425e509899a9fb0cc7c6d0799.
    
    Revert "Add `./` in front of local paths."
    
    This reverts commit b1449a846d6b0ef54e8ab15aa1c0216f9002cc89.
    
    Revert "Use reusable workflow for building releases."
    
    This reverts commit b12d198e06c4c72fb279f6e18b67e84cacdba55e.
    
    Revert "Cannot define `env`s in terms of other `env`s."
    
    This reverts commit 1988b09d6bda4fccb349afdb87a28d531c7509c0.
    
    Revert "Need to specify repo and ref explicitly now that workflow is could be called."
    
    This reverts commit f9e1176149bbde08d48f34f0d7404ed07c981466.
    
    Revert "Make workflow callable."
    
    This reverts commit 4eaa23dec8113a11f1d6989865a6c1a18dbdbb24.

--- release/getPytrnsysMasterGitSha.py
+++ release/getPytrnsysMasterGitSha.py
@@ -1,16 +1,10 @@
 import subprocess as _sp
-import sys as _sys
 
 
 def main() -> None:
-    if len(_sys.argv) != 3:
-        print(f"ERROR: Usage: {_sys.argv[0]} <repo url> <git ref>")
-        _sys.exit(-1)
-
-    url = _sys.argv[1]
-    gitRef = _sys.argv[2]
-
-    command = f"git ls-remote {url} {gitRef}".split()
+    command = (
+        "git ls-remote https://github.com/SPF-OST/pytrnsys.git master".split()
+    )
     completedProcess = _sp.run(
         command, check=True, capture_output=True, text=True
     )
