181276a2de3f62392a9bd4c1f7b58077338eb846
Damian Birchler <damian@damianbirchler.net>
2025-03-17 14:28:34 +0100

    Satisfy `mypy`.

--- release/spyder/start.py
+++ release/spyder/start.py
@@ -1,6 +1,6 @@
-import matplotlib_inline
+import matplotlib_inline  # type: ignore[import-untyped]
 
-import spyder.app.start as spyder
+import spyder.app.start as spyder  # type: ignore[import-untyped]
 
 # Make inline plots work
 # See here: https://github.com/spyder-ide/spyder/issues/22420#issuecomment-2562147200
